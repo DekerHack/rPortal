b2a28c686bca061e01386425629d9f5d71a63f48
Giovanni Buscarino <giovanni.buscarino@gmail.com>
2014-11-15 22:51:36 +0100

    update

--- js/app.js
+++ js/app.js
@@ -57,6 +57,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
+      .state('app.home', {
+          url: "/home",
+          views: {
+              'menuContent' :{
+                  templateUrl: "templates/home.html"
+              }
+          }
+      })
+
     .state('app.single', {
       url: "/playlists/:playlistId",
       views: {
@@ -67,6 +76,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
 
